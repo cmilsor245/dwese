0abf36ebf188e0529f47a34fd5e66c78da574e56
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-09 16:10:19 +0100

    commit

--- docker/www/relacion7/ej1/v3/login.php
+++ docker/www/relacion7/ej1/v3/login.php
@@ -1,3 +1,65 @@
 <?
-  
+  session_start();
+
+  include("includes/functions.php");
+
+  if (!isset($_SESSION["correct_credentials"])) {
+    $_SESSION["correct_credentials"] = false;
+  }
+
+  checkLoginFormVariables();
+
+  if (!$_SESSION["correct_credentials"]) {
+    if (isset($_SESSION["username-login"]) && isset($_SESSION["password-login"])) {
+      $username = $_SESSION["username-login"];
+      $password = hash("sha256", $_SESSION["password-login"]);
+
+      if ($username === "" || $password === "") {
+        unsetLoginVariables();
+        echo "
+          el usuario o la contraseña están vacíos
+          <a href = \"views/login.view.php\"><button>intentar de nuevo</button></a>
+          <a href = \"registro.php\"><button>registrarse</button></a>
+          <a href = \"index.php\"><button>landing page</button></a>
+        ";
+      } else {
+        try {
+          $host = "db";
+          $dbUsername = "root";
+          $dbPassword = "test";
+          $dbName = "first_db";
+          $conn = new PDO("mysql:host=$host;dbname=$dbName", $dbUsername, $dbPassword);
+          $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          $statement = $conn -> prepare("SELECT * FROM user WHERE username = :username AND password = :password LIMIT 1");
+          $statement -> execute(array(":username" => $username, ":password" => $password));
+          $resultado = $statement -> fetch();
+
+          if ($resultado) {
+            $_SESSION["correct_credentials"] = true;
+            $_SESSION["username-login"] = $username;
+            header("Location: contenido.php");
+          } else {
+            unsetLoginVariables();
+            echo "
+              el usuario no existe o la contraseña es incorrecta
+              <a href = \"views/login.view.php\"><button>intentar de nuevo</button></a>
+              <a href = \"registro.php\"><button>registrarse</button></a>
+              <a href = \"index.php\"><button>landing page</button></a>
+            ";
+          }
+        } catch (PDOException $e) {
+          echo "error: " . $e -> getMessage();
+        }
+      }
+    } else {
+      header("Location: views/login.view.php");
+    }
+  } else {
+    echo "
+      ya hay una sesión iniciada
+      <a href = \"contenido.php\"><button>ir al contenido</button></a>
+      <a href = \"index.php\"><button>landing page</button></a>
+    ";
+  }
 ?>
--- docker/www/relacion7/ej1/v3/registro.php
+++ docker/www/relacion7/ej1/v3/registro.php
@@ -1,3 +1,86 @@
 <?
-  
+  session_start();
+
+  include("includes/functions.php");
+
+  if (!isset($_SESSION["correct_credentials"])) {
+    $_SESSION["correct_credentials"] = false;
+  }
+
+  checkSignUpFormVariables();
+
+  if (!$_SESSION["correct_credentials"]) {
+    if (isset($_SESSION["username-signup"]) && isset($_SESSION["password-signup"]) && isset($_SESSION["password-signup-confirm"])) {
+      if ($_SESSION["password-signup"] === $_SESSION["password-signup-confirm"]) {
+        $username = $_SESSION["username-signup"];
+        $password = hash("sha256", $_SESSION["password-signup"]);
+
+        if ($username === "" || $password === "") {
+          unsetSignUpVariables();
+          echo "
+            el usuario o la contraseña están vacíos
+            <a href = \"views/registro.view.php\"><button>intentar de nuevo</button></a>
+            <a href = \"index.php\"><button>landing page</button></a>
+          ";
+        } else {
+          try {
+            $host = "db";
+            $dbUsername = "root";
+            $dbPassword = "test";
+            $dbName = "first_db";
+            $conn = new PDO("mysql:host=$host;dbname=$dbName", $dbUsername, $dbPassword);
+            $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $statement = $conn -> prepare("SELECT * FROM user WHERE username = :username LIMIT 1");
+            $statement -> execute(array(":username" => $username));
+            $resultado = $statement -> fetch();
+
+            if ($resultado) {
+              unsetSignUpVariables();
+              echo "
+                usuario ya existente
+                <a href = \"views/registro.view.php\"><button>intentar de nuevo</button></a>
+                <a href = \"index.php\"><button>landing page</button></a>
+              ";
+            } else {
+              $statement = $conn -> prepare("INSERT INTO user(username, password) values (:username, :password)");
+              $statement -> execute(array(
+                ":username" => $username,
+                ":password" => $password
+              ));
+
+              $statement = $conn -> prepare("SELECT * FROM user WHERE username = :username AND password = :password LIMIT 1");
+              $statement -> execute(array(":username" => $username, ":password" => $password));
+              $resultado = $statement -> fetch();
+
+              if ($resultado) {
+                $_SESSION["correct_credentials"] = true;
+                $_SESSION["username-login"] = $username;
+                header("Location: contenido.php");
+              } else {
+                echo "error inesperado al iniciar sesión después del registro.";
+              }
+            }
+          } catch (PDOException $e) {
+            echo "error: " . $e -> getMessage();
+          }
+        }
+      } else {
+        unsetSignUpVariables();
+        echo "
+          las contraseñas no coinciden
+          <a href = \"views/registro.view.php\"><button>intentar de nuevo</button></a>
+          <a href = \"index.php\"><button>landing page</button></a>
+        ";
+      }
+    } else {
+      header("Location: views/registro.view.php");
+    }
+  } else {
+    echo "
+      ya hay una sesión iniciada
+      <a href = \"contenido.php\"><button>ir al contenido</button></a>
+      <a href = \"cerrarsesion.php\"><button>cerrar sesión</button></a>
+    ";
+  }
 ?>
