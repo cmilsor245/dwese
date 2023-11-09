d3a3f949859267fb71bf82dd2f8615652019b800
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-09 12:15:50 +0100

    commit

--- docker/www/relacion7/ej1/v4/login.php
+++ docker/www/relacion7/ej1/v4/login.php
@@ -9,23 +9,57 @@
 
   checkLoginFormVariables();
 
-  if (isset($_SESSION["username-login"]) && isset($_SESSION["password-login"])) {
-    $username = $_SESSION["username-login"];
-    $password = hash("sha256", $_SESSION["password-login"]);
+  if (!$_SESSION["correct_credentials"]) {
+    if (isset($_SESSION["username-login"]) && isset($_SESSION["password-login"])) {
+      $username = $_SESSION["username-login"];
+      $password = hash("sha256", $_SESSION["password-login"]);
 
-    if ($username === "test" && $password === hash("sha256", "test")) {
-      $_SESSION["correct_credentials"] = true;
-      header("Location: contenido.php");
+      if ($username === "" || $password === "") {
+        unsetLoginVariables();
+        echo "
+          el usuario o la contraseña están vacíos
+          <a href = \"views/login.view.php\"><button>intentar de nuevo</button></a>
+          <a href = \"registro.php\"><button>registrarse</button></a>
+          <a href = \"index.php\"><button>landing page</button></a>
+        ";
+      } else {
+        try {
+          $host = "db";
+          $dbUsername = "root";
+          $dbPassword = "test";
+          $dbName = "first_db";
+          $conn = new PDO("mysql:host=$host;dbname=$dbName", $dbUsername, $dbPassword);
+          $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          $statement = $conn -> prepare("SELECT * FROM user WHERE username = :username AND password = :password LIMIT 1");
+          $statement -> execute(array(":username" => $username, ":password" => $password));
+          $resultado = $statement -> fetch();
+
+          if ($resultado) {
+            $_SESSION["correct_credentials"] = true;
+            $_SESSION["username-login"] = $username;
+            header("Location: contenido.php");
+          } else {
+            unsetLoginVariables();
+            echo "
+              el usuario no existe o la contraseña es incorrecta
+              <a href = \"views/login.view.php\"><button>intentar de nuevo</button></a>
+              <a href = \"registro.php\"><button>registrarse</button></a>
+              <a href = \"index.php\"><button>landing page</button></a>
+            ";
+          }
+        } catch (PDOException $e) {
+          echo "error: " . $e -> getMessage();
+        }
+      }
     } else {
-      unsetLoginVariables();
-      echo "
-        el usuario no existe o la contraseña es incorrecta
-        <a href = \"views/login.view.php\"><button>intentar de nuevo</button></a>
-        <a href = \"registro.php\"><button>registrarse</button></a>
-        <a href = \"index.php\"><button>landing page</button></a>
-      ";
+      header("Location: views/login.view.php");
     }
   } else {
-    header("Location: views/login.view.php");
+    echo "
+      ya hay una sesión iniciada
+      <a href = \"contenido.php\"><button>ir al contenido</button></a>
+      <a href = \"index.php\"><button>landing page</button></a>
+    ";
   }
 ?>
