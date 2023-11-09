8d3fbda80b0707f21734914c517b37612c308442
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-09 17:10:53 +0100

    commit

--- docker/www/relacion7/ej1/v4/registro.php
+++ docker/www/relacion7/ej1/v4/registro.php
@@ -23,47 +23,7 @@
             <a href = \"index.php\"><button>landing page</button></a>
           ";
         } else {
-          try {
-            $host = "db";
-            $dbUsername = "root";
-            $dbPassword = "test";
-            $dbName = "first_db";
-            $conn = new PDO("mysql:host=$host;dbname=$dbName", $dbUsername, $dbPassword);
-            $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $statement = $conn -> prepare("SELECT * FROM user WHERE username = :username LIMIT 1");
-            $statement -> execute(array(":username" => $username));
-            $resultado = $statement -> fetch();
-
-            if ($resultado) {
-              unsetSignUpVariables();
-              echo "
-                usuario ya existente
-                <a href = \"views/registro.view.php\"><button>intentar de nuevo</button></a>
-                <a href = \"index.php\"><button>landing page</button></a>
-              ";
-            } else {
-              $statement = $conn -> prepare("INSERT INTO user(username, password) values (:username, :password)");
-              $statement -> execute(array(
-                ":username" => $username,
-                ":password" => $password
-              ));
-
-              $statement = $conn -> prepare("SELECT * FROM user WHERE username = :username AND password = :password LIMIT 1");
-              $statement -> execute(array(":username" => $username, ":password" => $password));
-              $resultado = $statement -> fetch();
-
-              if ($resultado) {
-                $_SESSION["correct_credentials"] = true;
-                $_SESSION["username-login"] = $username;
-                header("Location: contenido.php");
-              } else {
-                echo "error inesperado al iniciar sesión después del registro.";
-              }
-            }
-          } catch (PDOException $e) {
-            echo "error: " . $e -> getMessage();
-          }
+          registerUser($username, $password);
         }
       } else {
         unsetSignUpVariables();
