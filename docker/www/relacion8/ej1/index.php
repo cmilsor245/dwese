bd3c9a4286274228c7fdd80774e212d231016720
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-16 13:30:00 +0100

    commit

--- docker/www/relacion8/ej1/index.php
+++ docker/www/relacion8/ej1/index.php
@@ -4,80 +4,83 @@
     <meta charset = "utf-8" />
     <meta name = "viewport" content = "width = device-width, initial-scale = 1.0" />
     <title>Gestclient</title>
+    <link rel = "stylesheet" href = "css/style.css" />
   </head>
   <body>
-    <div>
+    <div style = "background-color: lightblue;">
       <div>
-        <h1>GESTCLIENT</h1>
-        <h2>Client Management for CertiBank</h2>
+        <h1 class = "title">gestclient</h1>
+        <h2 class = "subtitle">gestión de clientes de certibank</h2>
 
         <?
-          // database connection
+          // conexión a la base de datos
 
-          // get the action of the button pressed in the form
+          // obtener la acción del botón que se ha pulsado en el formulario
 
-          // delete a client
-          if (isset($_GET["action"]) && $_GET["action"] == "delete") {
-            // make the appropriate database query
+          // dar de baja un cliente
+          if (isset($_GET["action"]) && $_GET["action"] == "remove") {
+            // hacer llamada a base de datos con la consulta oportuna
           }
 
-          // create a client
+          // dar de alta un cliente
           if (isset($_GET["action"]) && $_GET["action"] == "create") {
-            // make the appropriate database query
+            // hacer llamada a base de datos con la consulta oportuna
           }
 
-          // modify a client
+          // modificar un cliente
           if (isset($_GET["action"]) && $_GET["action"] == "modify") {
-            // make the appropriate database query
+            // hacer llamada a base de datos con la consulta oportuna
           }
 
-          // list
-          // this list is always displayed
-          // make the appropriate database query for the list of clients
-          $consulta;
+          // listado
+          // este listado se muestra siempre
+          // hacer llamada a base de datos con la consulta del listado de clientes
+          $query;
         ?>
 
         <table>
           <tr>
-            <th>DNI</th>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Phone</th>
+            <th>dni</th>
+            <th>nombre</th>
+            <th>dirección</th>
+            <th>teléfono</th>
             <th></th>
           </tr>
 
-          <form action = "index.php" method = "GET">
+          <form action = "index.php" method = "get">
             <tr>
-              <td><input type = "text" name = "dni" /></td>
-              <td><input type = "text" name = "name" /></td>
-              <td><input type = "text" name = "address" /></td>
-              <td><input type = "text" name = "phone" /></td>
-              <input type = "hidden" name = "action" value = "create" />
-              <td><input type = "submit" value = "add" /></td>
+              <td><input type = "text" name = "dni"></td>
+              <td><input type = "text" name = "name"></td>
+              <td><input type = "text" name = "address"></td>
+              <td><input type = "text" name = "phone"></td>
+              <input type = "hidden" name = "action" value = "create">
+              <td><input type = "submit" value = "añadir"></td>
             </tr>
           </form>
 
           <?
-            // display the clients from the database in the table
-            while ($registro = array()) { // modify the array and replace it with the appropriate code
-              echo "<tr>
-                <td>" . $registro["dni"] . " </td>
-                <td>" . $registro["name"] . " </td>
-                <td>" . $registro["address"] . " </td>
-                <td>" . $registro["phone"] . " </td>
-                <td>
-                  <a href = \"modify.php?&dni=" . $registro['dni'] . "&name=" . $registro['name'] . "&address=" . $registro['address'] . "&phone=" . $registro['phone'] . " \">
-                    <button>Modify</button>
-                  </a>
-                </td>
-                <td>
-                  <a href = \"index.php?action=delete&dni=" . $registro["dni"]."\">
-                    <button>
-                      <img width = \"20px\" src = \"img/papelera.png\" >
-                    </button>
-                  </a>
-                </td>
-              </tr>";
+            // mostrar los clientes de la base de datos en la tabla
+            while ($sign_up = array()) { // hay que modificar el array y cambiarlo por el código adecuado
+              echo "
+                <tr>
+                  <td>" . $sign_up["dni"] . "</td>
+                  <td>" . $sign_up["name"] . "</td>
+                  <td>" . $sign_up["address"] . "</td>
+                  <td>" . $sign_up["phone"] . "</td>
+                  <td>
+                    <a href = \"modificar.php?&dni=" . $sign_up["dni"] . "&name=" . $sign_up["name"] . "&address=" . $sign_up["address"] . "&phone=" . $sign_up["phone"] . "\">
+                      <button>modificar</button>
+                    </a>
+                  </td>
+                  <td>
+                    <a href = \"index.php?action=remove&dni=" . $sign_up["dni"] . "\">
+                      <button>
+                        <img width = \"20px\" src = \"img/papelera.png\" />
+                      </button>
+                    </a>
+                  </td>
+                </tr>
+              ";
             }
           ?>
         </table>
--- docker/www/relacion8/ej1/modificar.php
+++ docker/www/relacion8/ej1/modificar.php
@@ -4,22 +4,22 @@
     <meta charset = "utf-8" />
     <meta name = "viewport" content = "width = device-width, initial-scale = 1.0" />
     <title>Gestclient</title>
+    <link rel = "stylesheet" href = "css/style.css" />
   </head>
   <body>
-    <h1>GESTCLIENT</h1>
-    <h2>Client Management for CertiBank</h2>
-    <form action = "index.php" action = "GET">
+    <h1 class = "title">gestclient</h1>
+    <h2 class = "subtitle">gestión de clientes de certibank</h2>
+
+    <form action = "index.php" method = "get">
       <input type = "text" name = "dni" value = "<?=  $_GET["dni"] ?>">
       <input type = "text" name = "name" value = "<?=  $_GET["name"] ?>">
       <input type = "text" name = "address" value = "<?=  $_GET["address"] ?>">
       <input type = "text" name = "phone" value = "<?=  $_GET["phone"] ?>">
-      <input type = "hidden" name = "dniAntiguo" value = "<?=  $_GET["dni"] ?>">
+      <input type = "hidden" name = "previous-dni" value = "<?=  $_GET["dni"] ?>">
       <input type = "hidden" name = "action" value = "modify">
-      <br /><br />
-      <input type = "submit" value = "accept">
-      <a href = "index.php">
-        <button type = "button">cancel</button>
-      </a>
+      <br><br>
+      <input type = "submit" value = "aceptar">
+      <a href = "index.php"><button>cancelar</button></a>
     </form>
   </body>
 </html>
