76a8a38bf97c46476cf97fd7c16664039a3aba47
cmilsor245 <cmilsor245@g.educaand.es>
2023-11-25 12:48:34 +0100

    commit

--- docker/www/biblioteca/v2/index.php
+++ docker/www/biblioteca/v2/index.php
@@ -143,39 +143,23 @@
       function insertAuthorForm() {
         echo "<h1>insertar autor</h1>";
 
-        if (isset($_GET["name"]) && isset($_GET["last_name"]) && $_GET["name"] !== "" && $_GET["last_name"] !== "") {
-          $new_author_name = $_GET["name"];
-          $new_author_last_name = $_GET["last_name"];
+        $new_author_name = isset($_GET["name"]) ? $_GET["name"] : "";
+        $new_author_last_name = isset($_GET["last_name"]) ? $_GET["last_name"] : "";
 
-          echo "
-            <form method = \"get\" action = \"index.php\">
-              <label for = \"name\">nombre</label>
-              <input id = \"name\" type = \"text\" name = \"name\" value = \"$new_author_name\" autofocus autocomplete = \"off\" />
+        echo "
+          <form method = \"get\" action = \"index.php\">
+            <label for = \"name\">nombre</label>
+            <input id = \"name\" type = \"text\" name = \"name\" value = \"$new_author_name\" autofocus autocomplete = \"off\" onfocus = \"this.select()\" />
 
-              <label for = \"last_name\">apellido</label>
-              <input id = \"last_name\" type = \"text\" name = \"last_name\" value = \"$new_author_last_name\" autocomplete = \"off\" />
+            <label for = \"last_name\">apellido</label>
+            <input id = \"last_name\" type = \"text\" name = \"last_name\" value = \"$new_author_last_name\" autocomplete = \"off\" onfocus = \"this.select()\" />
 
-              <input type = \"hidden\" name = \"action\" value = \"insertAuthor\" />
+            <input type = \"hidden\" name = \"action\" value = \"insertAuthor\" />
 
-              <input type = \"submit\" value = \"insertar autor\" />
-            </form>
-          ";
-        } else {
-          echo "
-            <form method = \"get\" action = \"index.php\">
-              <label for = \"name\">nombre</label>
-              <input id = \"name\" type = \"text\" name = \"name\" autofocus autocomplete = \"off\" />
-
-              <label for = \"last_name\">apellido</label>
-              <input id = \"last_name\" type = \"text\" name = \"last_name\" autocomplete = \"off\" />
-
-              <input type = \"hidden\" name = \"action\" value = \"insertAuthor\" />
-
-              <input type = \"submit\" value = \"insertar autor\" />
-            </form>
-          ";
-        }
-      }
+            <input type = \"submit\" value = \"insertar autor\" />
+          </form>
+        ";
+      }    
 
       function insertAuthor($connection) {
         $new_author_name = $_GET["name"];
@@ -188,7 +172,7 @@
             if ($author["name"] === $new_author_name && $author["last_name"] === $new_author_last_name) {
               echo "
                 <h3>el autor ya existe</h3>
-                <a class = \"retry-button\" href = \"index.php?action=insertAuthorForm\"><button>intentarlo de nuevo</button></a>
+                <a class = \"retry-button\" href = \"index.php?action=insertAuthorForm&name=$new_author_name&last_name=$new_author_last_name\"><button>intentarlo de nuevo</button></a>
                 <a class = \"cancel-button\" href = \"index.php\"><button>cancelar</button></a>
               ";
               return;
@@ -218,24 +202,30 @@
       /* --------------------------------------------------------------------------------- */
 
       function insertBookForm($connection) {
-        echo "
-          <h1>insertar libro</h1>
+        echo "<h1>insertar libro</h1>";
 
+        $new_book_title = isset($_GET["title"]) ? $_GET["title"] : "";
+        $new_book_genre = isset($_GET["genre"]) ? $_GET["genre"] : "";
+        $new_book_country = isset($_GET["country"]) ? $_GET["country"] : "";
+        $new_book_year_published = isset($_GET["year_published"]) ? $_GET["year_published"] : "";
+        $new_book_num_pages = isset($_GET["num_pages"]) ? $_GET["num_pages"] : "";
+
+        echo "
           <form method = \"get\" action = \"index.php\">
             <label for = \"title\">titulo</label>
-            <input id = \"title\" type = \"text\" name = \"title\" autofocus autocomplete = \"off\" />
+            <input id = \"title\" type = \"text\" name = \"title\" value = \"$new_book_title\" autofocus autocomplete = \"off\" onfocus = \"this.select()\" />
 
             <label for = \"genre\" type = \"text\">género</label>
-            <input id = \"genre\" type = \"text\" name = \"genre\" autocomplete = \"off\" />
+            <input id = \"genre\" type = \"text\" name = \"genre\" value = \"$new_book_genre\" autocomplete = \"off\" onfocus = \"this.select()\" />
 
             <label for = \"country\" type = \"text\">país</label>
-            <input id = \"country\" type = \"text\" name = \"country\" autocomplete = \"off\" />
+            <input id = \"country\" type = \"text\" name = \"country\" value = \"$new_book_country\" autocomplete = \"off\" onfocus = \"this.select()\" />
 
-            <label for = \"year_published\" type = \"number\">año de punblicación</label>
-            <input id = \"year_published\" type = \"number\" name = \"year_published\" autocomplete = \"off\" />
+            <label for = \"year_published\" type = \"number\">año de publicación</label>
+            <input id = \"year_published\" type = \"number\" name = \"year_published\" value = \"$new_book_year_published\" autocomplete = \"off\" onfocus = \"this.select()\" />
 
             <label for = \"num_pages\" type = \"number\">número de páginas</label>
-            <input id = \"num_pages\" type = \"number\" name = \"num_pages\" autocomplete = \"off\" />
+            <input id = \"num_pages\" type = \"number\" name = \"num_pages\" value = \"$new_book_num_pages\" autocomplete = \"off\" onfocus = \"this.select()\" />
 
             <label for = \"author\" type = \"number\">autor</label>
             <select id = \"author\" name = \"author[]\" multiple>
@@ -244,9 +234,9 @@
         $authors_list = $connection -> query("SELECT * FROM author");
 
         while ($author = $authors_list -> fetch_assoc()) {
-          echo "
-            <option value = \"" . $author["author_id"] . "\">" . $author["name"] . " " . $author["last_name"] . "</option>
-          ";
+          $author_id = $author["author_id"];
+          $selected = in_array($author_id, isset($_GET["author"]) ? $_GET["author"] : []) ? "selected" : "";
+          echo "<option value = \"$author_id\" $selected>" . $author["name"] . " " . $author["last_name"] . "</option>";
         }
 
         echo "
@@ -260,21 +250,23 @@
       }
 
       function insertBook($connection) {
-        if (isset($_GET["title"]) && isset($_GET["genre"]) && isset($_GET["country"]) && isset($_GET["year_published"]) && isset($_GET["num_pages"]) && isset($_GET["author[]"]) && $_GET["title"] !== "" && $_GET["genre"] !== "" && $_GET["country"] !== "" && $_GET["year_published"] !== "" && $_GET["num_pages"] !== "" && $_GET["author[]"] !== "") {
-          $new_book_title = $_GET["title"];
-          $new_book_genre = $_GET["genre"];
-          $new_book_country = $_GET["country"];
-          $new_book_year_published = $_GET["year_published"];
-          $new_book_num_pages = $_GET["num_pages"];
+        $new_book_title = $_GET["title"];
+        $new_book_genre = $_GET["genre"];
+        $new_book_country = $_GET["country"];
+        $new_book_year_published = $_GET["year_published"];
+        $new_book_num_pages = $_GET["num_pages"];
+
+        if ($new_book_title !== "" && $new_book_genre !== "" && $new_book_country !== "" && $new_book_year_published !== "" && $new_book_num_pages !== "" && isset($_GET["author"]) && !empty($_GET["author"])) {
+          
         } else {
           echo "
             <h3>deben proporcionarse todos los datos del libro</h3>
-            <a class = \"retry-button\" href = \"index.php?action=insertBookForm\"><button>intentarlo de nuevo</button></a>
+            <a class = \"retry-button\" href = \"index.php?action=insertBookForm&title=$new_book_title&genre=$new_book_genre&country=$new_book_country&year_published=$new_book_year_published&num_pages=$new_book_num_pages\"><button>intentarlo de nuevo</button></a>
             <a class = \"cancel-button\" href = \"index.php\"><button>cancelar</button></a>
           ";
           return;
         }
-      }
+    }
 
       /* --------------------------------------------------------------------------------- */
 
