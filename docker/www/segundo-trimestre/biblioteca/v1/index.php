86c22906f8739e21e56efc319e8cd4ae6579621d
cmilsor245 <cmilsor245@g.educaand.es>
2024-01-05 19:44:46 +0100

    commit

--- docker/www/segundo-trimestre/biblioteca/v1/index.php
+++ docker/www/segundo-trimestre/biblioteca/v1/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset = "utf-8" />
     <meta name = "viewport" content = "width = device-width, initial-scale = 1.0" />
-    <title>bookstore v2</title>
+    <title>bookstore v1</title>
     <link rel = "stylesheet" href = "css/style.css" />
     <link rel = "preconnect" href = "https://fonts.googleapis.com">
     <link rel = "preconnect" href = "https://fonts.gstatic.com" crossorigin>
