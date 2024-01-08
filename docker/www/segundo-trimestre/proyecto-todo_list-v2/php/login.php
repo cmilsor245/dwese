2674fc2e50e89219331a31eb72d1de0c02aa6f98
cmilsor245 <cmilsor245@g.educaand.es>
2024-01-08 19:20:07 +0100

    commit

---
+++ docker/www/segundo-trimestre/proyecto-todo_list-v2/php/login.php
@@ -0,0 +1,25 @@
+<?
+  session_start();
+
+  include "includes/connection-functions.php";
+
+  $connection = generateDBConnection();
+
+  if (isset($_POST["login-username"]) && isset($_POST["login-password"])) {
+    if ($_POST["login-username"] === "" || $_POST["login-password"] === "") {
+      displayEmptyLoginInputErrorPage();
+    } else {
+      $username = $_POST["login-username"];
+
+      // correct way
+      $password = hash("sha256", $_POST["login-password"]);
+
+      // for testing
+      // $password = $_POST["login-password"];
+
+      checkCredentials($connection, $username, $password);
+    }
+  } else {
+    displayIncorrectLoginCredentialsErrorPage();
+  }
+?>
