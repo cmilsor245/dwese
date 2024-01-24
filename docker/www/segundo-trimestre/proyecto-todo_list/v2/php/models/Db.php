d28a8efa0c0535520bce560882d8e9171a512e0b
cmilsor245 <cmilsor245@g.educaand.es>
2024-01-24 15:45:32 +0100

    resetting structure for first mvc project

--- docker/www/segundo-trimestre/proyecto-todo_list/v2/php/models/Db.php
+++ docker/www/segundo-trimestre/proyecto-todo_list/v2/php/models/Db.php
@@ -3,7 +3,7 @@
     private $db;
 
     public function __construct() {
-      require_once('config.php');
+      require_once 'config.php';
       $this -> db = new mysqli(DB_HOST, DB_USER, DB_PASS, DBNAME);
       if ($this -> db -> connect_errno) return -1;
       else return 0;
